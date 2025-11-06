38af9cc4e64ab5d7bdd9ff3d8ce3ef630860c32d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-11-06 16:21:19 +0000

    Add auth case

---
+++ test/support/auth_case.ex
@@ -0,0 +1,36 @@
+defmodule DrywWeb.AuthCase do
+  @moduledoc """
+  This module defines the test case to be used by tests that require setting up
+  authentication.
+
+  This example is developed for a typical resource "User" and "Item", and uses
+  AshAuthentication magic link strategy, with a default setup of user email
+  address. Modify this example as you wish for your own needs and settings.
+
+  This code is extracted from contactopensource/navatrack and is public domain.
+  I appreciate any constructive user_review and advice on how to improve this code.
+  And I'm happy to answer questions. Email joel@joelparkerhenderson.com.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      alias Dryw.Accounts.User
+
+      def my_user!() do
+        User
+        |> Ash.Changeset.for_create(:create, %{email: "example@example.com"})
+        |> Ash.create!(upsert?: true, upsert_identity: :unique_email)
+      end
+
+      def my_sign_in!(user) do
+        strategy = AshAuthentication.Info.strategy!(User, :magic_link)
+        {:ok, token} = AshAuthentication.Strategy.MagicLink.request_token_for(strategy, user)
+        # TODO carp
+        {:ok, user} = AshAuthentication.Strategy.action(strategy, :sign_in, %{"token" => token})
+        user
+      end
+    end
+  end
+end
